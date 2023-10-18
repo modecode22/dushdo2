640776c6bd1eb2a6af26d90b8043f3af2bd9dc44
moncef <epiccoder00@gmail.com>
2023-10-18 23:50:59 +0100

    added task bar to the home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,9 +1,11 @@
+import KanbanBoard from "../components/KanbanBoard";
 import UiPage from "../components/ui/ui-page";
 
 const Home = () => {
   
   return (
     <section className="flex-grow  h-full  ">
+      <KanbanBoard />
 <UiPage />
     </section>
   );
