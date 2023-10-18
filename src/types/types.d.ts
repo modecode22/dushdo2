4ae7ea8aa61420bb6fb11c93e393ff8c1006c243
moncef <epiccoder00@gmail.com>
2023-10-18 23:51:09 +0100

    added the types of the tasks

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -5,3 +5,15 @@ declare global {
   }
 
   type GradioResponse ={data: [{name:string, data: string , size?: number, is_file?:boolean, orig_name?:string},srting]}
+   type Id = string | number;
+
+ type Column = {
+  id: Id;
+  title: string;
+};
+
+ type Task = {
+  id: Id;
+  columnId: Id;
+  content: string;
+};
