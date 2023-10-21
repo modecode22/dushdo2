54b2e48fbe2c4266cbceab3a855aead9d6aa9033
moncef <epiccoder00@gmail.com>
2023-10-21 23:51:53 +0100

    removed some not nedded types

--- src/types/settings.d.ts
+++ src/types/settings.d.ts
@@ -4,21 +4,8 @@ type ThemeArray = [
     { option: "light"; name: "light" },
     { option: "dark"; name: "dark" }
   ];
-  type Language = string
-  
-  type LanguageArray = Language[]
-
 
   type ContextType = {
     theme: ThemeType;
     updateTheme: (theme: ThemeType) => void;
-    fromLanguage: string;
-    updateFromLanguage: (language: string) => void;
-    toLanguage: string;
-    updateToLanguage: (language: string) => void;
-    outputDevice: string | undefined;
-    updateOutputDevice: (device: string) => void;
-    inputDevice: string | undefined;
-    updateInputDevice: (device: string) => void;
-    devices: MediaDeviceInfo[];
   };
